ea653e08994432f20a4f6fb5ba0e3afa67406ead
Mark-Daniel-Callangan <callanganmark89@gmail.com>
2025-07-30 14:00:01 +0000

    Count_Number_of_word

---
+++ Count_Number_of_word.java
@@ -0,0 +1,21 @@
+
+    import java.util.Scanner;
+
+public class Count_Number_of_word {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        
+        if (input.trim().isEmpty()) {
+            System.out.println("Number of words: 0");
+        } else {
+            String[] words = input.trim().split("\\s+");
+            System.out.println("Number of words: " + words.length);
+        }
+    }
+}
