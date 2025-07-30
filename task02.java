6a8dfbd9d16c59f14abff219298d539cd0271d44
Mark-Daniel-Callangan <callanganmark89@gmail.com>
2025-07-30 14:34:07 +0000

    tasking02

---
+++ task02.java
@@ -0,0 +1,15 @@
+public class task02 {
+    public static void main(String[] args) {
+        char h = 'H';
+        byte three = 3;
+        short one = 1;
+        int zero = 0;
+        String space = " ";
+        String world = "world";
+        float twoPointZero = 2.0f;
+        boolean tru = true;
+
+        String output = "" + h + three + one + zero + space + world + twoPointZero + space + tru;
+        System.out.println(output);
+    }
+}
